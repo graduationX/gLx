4383cf8395720ecc77960e51488efc3655d504ce
lin <lin@DESKTOP-BO6T7G1>
2018-01-10 17:36:30 +0800

    导入前端模板

--- knowledge_educate/src/main/java/com/educate/front/controller/PersonController.java
+++ knowledge_educate/src/main/java/com/educate/front/controller/PersonController.java
@@ -24,7 +24,6 @@ public class PersonController {
 	@Autowired
 	private PersonService personService;
 	
-	
 	@RequestMapping("personList")
 	public String personList(Model model){
 		List<Person> list =new ArrayList<>();
