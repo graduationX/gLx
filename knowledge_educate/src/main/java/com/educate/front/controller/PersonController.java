3b6e7c66eb66f4d24ff61eb77c958e0037eaaa63
lin <lin@DESKTOP-BO6T7G1>
2018-01-12 16:43:15 +0800

    S

--- knowledge_educate/src/main/java/com/educate/front/controller/PersonController.java
+++ knowledge_educate/src/main/java/com/educate/front/controller/PersonController.java
@@ -3,9 +3,14 @@ package com.educate.front.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,7 +33,8 @@ public class PersonController {
 	private PersonService personService;
 	
 	@RequestMapping(value = "/personList", method = RequestMethod.GET)
-	public @ResponseBody String personList(Model model){
+	public @ResponseBody String personList(HttpSession session,HttpServletRequest request,
+			HttpServletResponse response){
 		List<Person> list =new ArrayList<>();
 		list =personService.selectlist();
 		return  JSON.toJSONString(list);
